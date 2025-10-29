d7e104afd927a299c2bfec991f094135707e7866
Karl Klingler <klikar3@web.de>
2025-10-29 12:14:44 +0100

    -- add Notes and rm some .bak files
    Signed-off-by: Karl Klingler <klikar3@web.de>

---
+++ sql/GRANTS.sql
@@ -0,0 +1,5 @@
+GRANT SHOW VIEW ON  wt_data.* TO 'wtdata'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, ALTER ON  wt_data.* TO 'wtdata'@'localhost';
+
+-- (GRANT ALL PRIVILEGES ON wt_data.* TO 'wtdata'@'localhost';)
+
